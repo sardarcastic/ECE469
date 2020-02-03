302b8816f7fb263f3350a8f8e426ae2010918990
Moiz S Rasheed <mrasheed@ecegrid-thin5.ecn.purdue.edu>
2020-02-03 14:48:28 -0500

    q2 producer

---
+++ lab2/apps/q2/include/spawn.h
@@ -0,0 +1,13 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+
+typedef struct circular_buffer {
+  int head;
+  int tail;
+  char * buffer; 
+} circular_buffer;
+
+#define PRODUCER_TO_RUN "producer.dlx.obj"
+#define CONSUMER_TO_RUN "consumer.dlx.obj"
+
+#endif
